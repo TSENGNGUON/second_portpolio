f6fe1fea23f97df44049e3820d1a918a05b6593f
Tun Sreynouch <sengn5112@gmail.com>
2024-08-18 22:20:44 +0700

    adding a sm responsive in tailwind and css min-width

--- src/components/Portpolio.jsx
+++ src/components/Portpolio.jsx
@@ -95,17 +95,17 @@ function LeftSide() {
 
 
   return (
-    <div id='about' className='w-full main'>
-        <div className='w-2/4 flex fixed flex-col items-center justify-center h-screen  text-lg overflow-hidden'>
+    <div id='about' className='w-full  sm:flex sm:flex-col '>
+        <div className='w-2/4 flex fixed flex-col items-center justify-center h-screen  text-lg overflow-hidden sm:w-full sm:h-auto sm:relative sm:mt-10'>
       
-        <div className='absolute top-[120px]'>
+        <div className='absolute top-[120px] sm:relative sm:top-[0px]  sm:w-full sm:mb-36 sm:ml-10'>
         <h1 className='font-bold text-5xl text-portfolio mb-4'>Tun Seng Nguon</h1>
-        <h4 className='font-semibold text-2xl text-portfolio mb-4'>Junior Frontend Developer</h4>
-        <p className='text-normal'>I build a Frontend project, and <br />
+        <h4 className='font-semibold text-2xl text-portfolio mb-4 sm:text-xl'>Junior Frontend Developer</h4>
+        <p className='text-normal sm:text-lg'>I build a Frontend project, and <br />
         accessible digital experiences.
         </p>
         </div>
-        <div className='absolute left-48 mt-48'>
+        <div className='absolute left-48 mt-48 sm:hidden'>
             <ul>
 
                 {items.map((item, index) => (
@@ -123,7 +123,7 @@ function LeftSide() {
 
             </ul>
         </div>
-        <div className='absolute bottom-16 left-48'>
+        <div className='absolute bottom-16 left-48  sm:w-full sm:relative sm:bottom-28 sm:mr-[340px]'>
             <ul className='flex'>
                 <li className='pr-5'><a href="#" className='text-normal text-2xl' title="Github"><FaGithub/></a></li>
                 <li className='pr-5'><a href="#" className='text-normal text-2xl' title="Instagram" ><FaSquareInstagram/></a></li>
@@ -133,31 +133,33 @@ function LeftSide() {
         
     </div>
     {/* right side */}
-    <div  className='w-2/4 mt-[120px] rightside overflow-auto ml-[50%]'> 
+    <div  className='w-2/4 mt-[120px] rightside overflow-auto ml-[50%] sm:ml-6 sm:w-full  sm:mt-0 '> 
       {/* about section */} 
-        <div className='text-pright font-sans font-semibold text-lg'>
+        <div className="sm:text-sm sm:font-bold sm:text-white sm:mb-9">ABOUT</div>
+        <div className='text-pright font-sans font-semibold text-lg sm:text-[16px]'>
         {
             contents.map((content)=>(
-                <p key={content.id} className='w-9/12 pb-4'>{content.content}</p>
+                <p key={content.id} className='w-9/12 pb-4 sm:w-[93%]'>{content.content}</p>
             ))
         }
         </div>
     {/* experience section */}
         <div id='experience' className='text-normal'>
+            <div className='sm:text-sm sm:font-bold sm:text-white sm:mb-9 sm:mt-14'>EXPERIENCE</div>
             <ul >
                 {
                     modal.map((item)=>(
                     <li key={item.id}>
                     <a href="#">
                     {/* modal */}
-                    <div id='box' className='w-[75%] rounded-md p-3 flex flex-row'>
+                    <div id='box' className='w-[75%] sm:w-[93%]  rounded-md  sm:pt-3 sm:pb-3 sm:pr-3 flex flex-row '>
                         {/* header */}
-                        <div className='w-2/4'>
-                        <p className='w-full text-normal flex items-center'>{item.startYear} <div className='w-5 h-[1px] bg-normal rounded-full mx-1'></div> {item.endYear}</p>
+                        <div className='w-2/4  sm:w-1/5 '>
+                        <p className='w-full text-normal flex items-center sm:text-sm'>{item.startYear} <div className='w-5 h-[1px] bg-normal rounded-full mx-1'></div> {item.endYear}</p>
                         </div>
                         {/* body */}
-                        <div className='w-[70%]'>
-                            <h4 className='flex text-nowrap font-semibold text-lg items-center text-portfolio'><p className='pr-1'>{item.title}</p> <FiExternalLink/></h4>
+                        <div className='w-[60%] sm:w-[100%]  sm:pl-10 '>
+                            <h4 className='flex text-nowrap font-semibold text-lg sm:text-[16px] items-center text-portfolio'><p className='pr-1 sm:relative sm:bottom-1'>{item.title}</p> <p className='sm:relative sm:bottom-1'><FiExternalLink/></p></h4>
 
                             <p className='mt-2 content'>{item.content}</p>
                         
@@ -166,9 +168,9 @@ function LeftSide() {
                             <ul className='flex mt-8'>
 
                             {item.technologies.map((child)=>(
-                                <li key={child.id} className='subject py-2 px-3 bg-btn rounded-full li-btn mr-1'>
+                                <li key={child.id} className='subject flex items-center sm:py-1 sm:px-2 bg-btn rounded-full li-btn mr-1 '>
 
-                                <a className='text-titlebtn font-semibold text-sm a-btn'
+                                <a className='text-titlebtn font-semibold text-sm sm:text-[12px] a-btn '
                                 href="#">
                                     {child.subjectList}
                                 </a>
@@ -187,25 +189,26 @@ function LeftSide() {
         </div>
         {/* view full resume */}
        <div className='resume mt-5'>
-       <a href={`${MyCv}`} id='resume' className='text-white flex items-center text-xl'><p className='mr-2'>View Full CV</p> <p> <FiExternalLink/></p></a>
+       <a href={`${MyCv}`} id='resume' className='text-white flex items-center text-xl'><p className='mr-2 sm:text-sm sm:font-bold sm:text-white sm:mb-9 sm:mt-14'>View Full CV</p> <p className='sm:text-sm sm:font-bold sm:text-white sm:mb-9 sm:mt-14'> <FiExternalLink/></p></a>
        </div>
 
        {/* Project Section */}
-       <div id='project' className='text-normal mt-40'>
+       <div id='project' className='text-normal mt-40 '>
+        <div className='sm:text-sm sm:font-bold sm:text-white sm:mb-9 sm:mt-14'>PROJECTS</div>
             <ul >
                 {
                     modalProject.map((item)=>(
                     <li key={item.id}>
                     <a target='_blank' href={item.url}>
                     {/* modal */}
-                    <div id='box' className='w-[75%] rounded-md p-3 flex flex-row'>
+                    <div id='box' className='w-[75%]  sm:w-[93%] rounded-md sm:pb-3 sm:pr-3 sm:pt-3 flex flex-row'>
                         {/* header */}
-                        <div className='w-2/4'>
-                        <p className='w-full text-normal flex items-center'><img className='w-50 rounded-md' src={item.img}  /></p>
+                        <div className='w-2/4   sm:w-[30%]'>
+                        <p className='w-[100%] text-normal flex items-center'><img className='sm:w-[100%] rounded-md' src={item.img}  /></p>
                         </div>
                         {/* body */}
-                        <div className='w-[70%]'>
-                            <h4 className='flex text-nowrap font-semibold text-lg items-center text-portfolio'><p className='pr-1'>{item.title}</p> <FiExternalLink/></h4>
+                        <div className='w-[70%] sm:w-[100%]  sm:pl-10'>
+                            <h4 className='flex text-nowrap font-semibold text-lg items-center text-portfolio'><p className='pr-1 sm:text-[16px] sm:relative sm:bottom-2'>{item.title}</p> <p className='sm:relative sm:bottom-2'><FiExternalLink/></p> </h4>
 
                             <p className='mt-2 content'>{item.content}</p>
                         
@@ -214,9 +217,9 @@ function LeftSide() {
                             <ul className='flex mt-8'>
 
                             {item.technologies.map((child)=>(
-                                <li key={child.id} className='subject py-2 px-3 bg-btn rounded-full li-btn mr-1'>
+                                <li key={child.id} className='subject flex items-center  bg-btn rounded-full li-btn mr-1 sm:py-1 sm:px-2'>
 
-                                <a className='text-titlebtn font-semibold text-sm a-btn'
+                                <a className='text-titlebtn font-semibold text-sm a-btn sm:text-[12px]'
                                 href="#">
                                     {child.subjectList}
                                 </a>
